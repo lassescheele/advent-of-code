148df3efffa3b4905a71f89de3fdeded2bb9aba1
lscheele <lassescheele@yahoo.de>
2019-12-04 00:16:50 +0100

    Update day-02.py

--- 2019/day-02/day-02.py
+++ 2019/day-02/day-02.py
@@ -1,37 +1,28 @@
-import math as m
 import pandas as pd
 
-intcode_df = pd.read_csv("input.csv", sep=",", header=None)
-intcode_array = intcode_df.iloc[0,:].values
+initial_memory = pd.read_csv("input.csv", sep=",", header=None).iloc[0,:].values
 
 # Part 1
-def run_computer(array):
-    index_opcodes_list = [i for i in range(0,len(array),4)]
-    for index_opcode in index_opcodes_list:
-        opcode = array[index_opcode]
+def run_intcode_program(noun, verb):
+    memory = initial_memory.copy()
+    memory[1:3] = [noun, verb]
+    pointer_opcodes = [i for i in range(0,len(memory),4)]
+    for pointer_opcode in pointer_opcodes:
+        opcode = memory[pointer_opcode]
         if opcode == 99:
             break
         elif opcode == 1:
-            array[array[index_opcode+3]] = array[array[index_opcode+1]] + array[array[index_opcode+2]]
+            memory[memory[pointer_opcode+3]] = memory[memory[pointer_opcode+1]] + memory[memory[pointer_opcode+2]]
         elif opcode == 2:
-            array[array[index_opcode+3]] = array[array[index_opcode+1]] * array[array[index_opcode+2]]
-    return(array)
-
-intcode_array_part1 = intcode_array.copy()
-intcode_array_part1[1:3] = [12,2]
-print("Part 1: ",run_computer(intcode_array_part1)[0])
+            memory[memory[pointer_opcode+3]] = memory[memory[pointer_opcode+1]] * memory[memory[pointer_opcode+2]]
+    return(memory[0])
+print("Part 1: ",run_intcode_program(12, 2))
 
 # Part 2
-goal = 19690720
 find = False
-for noun in range(0,99):
-    print(noun)
-    for verb in range(0,99):
-        intcode_array_part1 = intcode_array.copy()
-        intcode_array_part1[1:3] = [noun,verb]
-        result = run_computer(intcode_array_part1)[0]
-        if result == goal:
-            print(noun, verb, result)
+for noun in range(0,100):
+    for verb in range(0,100):
+        if run_intcode_program(noun, verb) == 19690720:
             find = True 
             break
     if find: 
